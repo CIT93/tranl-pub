c3a3afd96cbd9c7adb9e713291f432d190af4377
Tran Le <0961419@my.scccd.edu>
2024-08-24 19:09:07 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,21 @@
-console.log('Hello World!')
+console.log("hello from inside the main.js file")
+
+// let myVar;
+
+const myVar = "Tran";
+const myVarType = typeof(myVar);
+console.log("myVarType" + myVarType);
+console.log(`myVarType" + ${myVarType}`);
+
+
+
+function runNow () {
+    if(myVarType === "number") {
+        console.log(`will 1 this one run?`);
+    } else {
+        console.log(`will 2 this one run`);
+    }
+}
+
+runNow();
+runNow();
