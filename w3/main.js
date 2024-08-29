002ec045e557c90bea2085e96c76fc083db1693f
Tran Le <0961419@my.scccd.edu>
2024-08-28 22:22:54 -0700

    functions

--- w3/main.js
+++ w3/main.js
@@ -1,24 +1,28 @@
-
-if (numberInHousehold === 1) {
-    carbonFootprintPoints = carbonFootprintPoints + 14;
-} else if (numberInHousehold === 2) {
-    carbonFootprintPoints = carbonFootprintPoints + 12;
-} 
-else if (numberInHousehold === 3) {
-    carbonFootprintPoints = carbonFootprintPoints + 10;
-}
-else if (numberInHousehold === 4) {
-    carbonFootprintPoints = carbonFootprintPoints + 8;
-}
-else if (numberInHousehold === 5) {
-    carbonFootprintPoints = carbonFootprintPoints + 6;
-}
-else if (numberInHousehold === 6) {
-    carbonFootprintPoints = carbonFootprintPoints + 4;
-} else if (numberInHousehold > 6) {
-    carbonFootprintPoints = carbonFootprintPoints + 2;
-}
-else {
-    console.log.print('Based on the numer of member of household of ${numberInHousehold} the points would be ${carbonFootprintPoints).');
+function TestFunction(numberInHousehold) {
+    console.log("Inside the function")
+    if (numberInHousehold === 1) {
+        carbonFootprintPoints = carbonFootprintPoints + 14;
+    } else if (numberInHousehold === 2) {
+        carbonFootprintPoints = carbonFootprintPoints + 12;
+    } else if (numberInHousehold === 3) {
+        carbonFootprintPoints = carbonFootprintPoints + 10;
+    } else if (numberInHousehold === 4) {
+        carbonFootprintPoints = carbonFootprintPoints + 8;
+    } else if (numberInHousehold === 5) {
+        carbonFootprintPoints = carbonFootprintPoints + 6;
+    } else if (numberInHousehold === 6) {
+        carbonFootprintPoints = carbonFootprintPoints + 4;
+    } else if (numberInHousehold > 6) {
+        carbonFootprintPoints = carbonFootprintPoints + 2;
+    } 
+    console.log('Based on the numer of member of household of ${numberInHousehold} the points would be ${carbonFootprintPoints).');
 }
 
+let carbonFootprintPoints = 0;
+// const numberInHousehold = 3;
+
+
+// global scope
+
+TestFunction(2)
+TestFunction(3)
