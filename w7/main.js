3f21d8b935a7bbab72567abc5461f8f210e978ff
Tran Le <0961419@my.scccd.edu>
2024-09-24 21:23:57 -0700

    first challenge and adding the style.css

--- w7/main.js
+++ w7/main.js
@@ -1,44 +1,12 @@
 import { renderTbl } from "./render.js";
+import { determineHouseSizePts, determineHouseHoldPts } from "./carbonpoints.js";
 
 
 const FORM = document.getElementById("form");
 const OUTPUT = document.getElementById("output");
 const cfpData = [];
 
-function determineHouseSizePts(Housesize) {
-    let carbonFootprintPoints = 0;
-    if ( Housesize === "large") {
-        carbonFootprintPoints = 10;
-    } else if (Housesize === "medium") {
-        carbonFootprintPoints = 7;
-    } else if (Housesize === "small") {
-        carbonFootprintPoints = 4;
-    } else if (Housesize === "apartment") {
-        carbonFootprintPoints = 2;
-    }
-    return carbonFootprintPoints;
-}
 
-
-function determineHouseHoldPts(numberInHousehold) {
-    let houseHoldPoints = 0;
-    if (numberInHousehold === 1) {
-        houseHoldPoints = 14;
-    } else if (numberInHousehold === 2) {
-        houseHoldPoints = 12;
-    } else if (numberInHousehold === 3) {
-        houseHoldPoints = 10;
-    } else if (numberInHousehold === 4) {
-        houseHoldPoints = 8;
-    } else if (numberInHousehold === 5) {
-        houseHoldPoints = 6;
-    } else if (numberInHousehold === 6) {
-        houseHoldPoints =4;
-    } else if (numberInHousehold > 6) {
-        houseHoldPoints = 2;
-    } 
-    return houseHoldPoints
-}
 
 function displayOutObj(obj) {
     console.log(obj);
