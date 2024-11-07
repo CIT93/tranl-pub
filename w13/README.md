89da0b72c446e6c6273710181710ad49f7c43e7c
Tran Le <0961419@my.scccd.edu>
2024-11-06 19:45:13 -0800

    w13 my attempt at the code, before I review other students or solution codes

--- w13/README.md
+++ w13/README.md
@@ -8,3 +8,9 @@
 - I finish all the const for a basic event listeners, based on previous week's codes.
 - I'm now attempting to do the setTimeout, using the exampled provided code in the website you gave us. 
 - The setTimeOut works and the display works as well, with no error so far so I'll keep what I have right now and commit. 
+
+## Week 13 Coding Challenge
+- I start by adding the promise into the const startExcerise function and embedded the rest of the other codes into it. Adding a resolve() right before the setTimeout timer.
+- I then move to the eventListener, under the startExerise, start writing the .then and write the condition for it, moving next to the .catch(error => )
+- I started to write the promise condition, the resolve works but but i'm not sure how to impliment the reject, will have to look into that
+- I will update what i have and wait for feedback
--- w13/main.js
+++ w13/main.js
@@ -1,18 +1,26 @@
 const FORM = document.getElementById("form");
 
 const startExcerise = (exerciseType, number, time) => {
+    return new Promise((resolve) => {
     const output = document.getElementById("output");
     output.textContent = `Based on your ${exerciseType}, your ${number} numbers of Reps, this is the amount of ${time} time to complete`; 
     setTimeout(() => {
        output.textContent = "STOP! You've have done enough for your session"
+       resolve();
       }, time * 5000);
-};
-
+});
+}
 FORM.addEventListener("submit", (e) => {
     e.preventDefault();
     const exerciseType = FORM.exerciseType.value;
     const Repsnumber = FORM.Repsnumber.value;
     const Time = FORM.Time.value;
     startExcerise(exerciseType, Repsnumber, Time)
+    .then(() => {
+        console.log("Your excerise is completed.");
+    })
+    .catch(error => {
+       console.log(`ERROR: ${error}`);
+    });
     FORM.reset();
 });
