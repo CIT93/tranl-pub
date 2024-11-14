1d9e1c83e99e6efd101c87dc0f0725d718db65d6
Tran Le <0961419@my.scccd.edu>
2024-11-13 20:47:46 -0800

    w14 my attempt ath the code, before I review other students or solution code

--- w14/main.js
+++ w14/main.js
@@ -1,1 +1,32 @@
+function renderPhotos(photos) {
+    photos.forEach((img) => {
+        const imgEl = document.createElement("img")
+        imgEl.setAttribute("src", img.thumbnailURL);
+        document.getElementById("output").appendChild(imgEl);
+    })
+}
 
+async function getPhotos() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/photos')
+        const data = await response.json('');
+        onSuccess(data)
+    } 
+    catch(error) {
+        console.log(`Error`, error);
+    }
+}
+
+async function start() {
+    getPhotos()
+}
+
+function onSuccess(photos) {
+    renderPhotos(photos);
+}
+ 
+function onError(error) {
+    console.log(`Error`, error);
+}
+
+start();
---
+++ w14/readme.md
@@ -0,0 +1,5 @@
+## Notes
+- Starting with the program, I copy down the function that you had already made in the video
+- Then I notice the output element so I figured there must be a div for it to display in the index.html so I added that
+- I then create most of the structure based on the video instruction and the codes we already have on pri repo.
+- It manage to rectrieve the photos but did not render properly so I don't really know how to fix that, I'll submit and wait for feedback then watch the solutions video. 
