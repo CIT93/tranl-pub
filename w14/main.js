f9577e11b1e18fed118e3529a9a470197b25c15f
Tran Le <0961419@my.scccd.edu>
2024-11-14 17:11:41 -0800

    fix code after watching solutions

--- w14/main.js
+++ w14/main.js
@@ -1,32 +1,38 @@
 function renderPhotos(photos) {
     photos.forEach((img) => {
         const imgEl = document.createElement("img")
-        imgEl.setAttribute("src", img.thumbnailURL);
+        imgEl.setAttribute("src", img.thumbnailUrl);
         document.getElementById("output").appendChild(imgEl);
     })
 }
 
 async function getPhotos() {
     try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/photos')
-        const data = await response.json('');
-        onSuccess(data)
+        const data= await fetch('https://jsonplaceholder.typicode.com/photos')
+        const photos = await data.json('');
+    if (!photos.length) {
+        console.log('error on return value')
+    } else {
+        onSuccess(photos)
+    }
     } 
     catch(error) {
-        console.log(`Error`, error);
+        onError(error)
     }
+}
+
+function onSuccess(photos) {
+    renderPhotos(photos);
+}
+
+function onError(error) {
+    console.log(`Error`, error);
 }
 
 async function start() {
     getPhotos()
 }
 
-function onSuccess(photos) {
-    renderPhotos(photos);
-}
- 
-function onError(error) {
-    console.log(`Error`, error);
-}
+
 
 start();
